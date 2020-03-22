24ade09a436f9fb3ae265e85f312929b864175ee
Matthew Korn <mkorn99@bu.edu>
2020-03-22 13:31:15 -0400

    Initial commit

---
+++ hello_world/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+# Create your views here.
+import time
+
+def homePageView(request):
+    '''Respons to an HTTP request with single web page'''
+
+    response_html = '''
+    <html>
+    <h1>Hello, world!</h1>
+    <p>
+    <hr>
+    This page was generate at %s.
+    </html>
+    ''' % time.ctime()
+
+    return HttpResponse('Hello, world!')
