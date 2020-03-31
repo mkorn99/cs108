caf76bd70ec5eab596f4d4dfc66dc4de775cb751
Matthew Korn <mkorn99@bu.edu>
2020-03-30 20:23:42 -0400

    Example 16

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,10 +2,10 @@
 #description: direct URL requests to view functions
 
 from django.urls import path
-from .views import HomePageView, QuotePageView, RandomQuotePageView
-
+from .views import *
 urlpatterns = [
     path('', RandomQuotePageView.as_view(), name="random"),
     path('all', HomePageView.as_view(), name='home'),
     path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), 
+    path('person/<int:pk>', PersonPageView.as_view(), name='person'), 
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Quote
+from .models import Quote, Person, Image
 from django.views.generic import ListView, DetailView
 import random
 
@@ -34,5 +34,12 @@ class RandomQuotePageView(DetailView):
         r = random.randint(0, len(all_quotes) -1)
         q = all_quotes[r]
         return q
+
+class PersonPageView(DetailView):
+    '''Show all quotes and all images for one person'''
+
+    model = Person
+    template_name = 'quotes/persons.html'
+    context_object_name = 'person'
 
          
